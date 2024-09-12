521635c832588bc6f6d035b89d103aea868eecb5
arpad-varga90 <arpad.varga90@gmail.com>
2024-09-12 16:53:02 +0200

    create server.js to have communication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.port;
+
+const app = express();
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
