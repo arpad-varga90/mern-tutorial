672a6f781b6449acae0b4ea122f177bb79604dbf
arpad-varga90 <arpad.varga90@gmail.com>
2024-09-24 14:03:50 +0200

    add else statement

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,10 @@ if (process.env.NODE_ENV === "production") {
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
   );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
